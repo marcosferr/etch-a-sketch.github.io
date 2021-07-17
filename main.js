28bccf6c9419bdb7c56a079508ca2d371285ab8f
Marcos Ferreira <ferreiramarcosadrian@gmail.com>
2021-07-17 17:30:22 -0400

    Write the etch a sketch 1.0 added index.html and main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+const container = document.querySelector("#container");
+let size = 16;
+let width=1000;
+const clearBtn = document.querySelector('#clearBtn');
+clearBtn.addEventListener('click',function(ev){newGrid()});
+
+
+function setGrid(){
+for(i=0; i<(size*size);i++){
+let element = document.createElement('div');
+element.classList.add('square');
+element.style.width= `${width/size}px` ;
+element.style.height = `${width/size}px`;
+element.addEventListener('mouseover',function(ev){ 
+    this.classList.add('black');
+});
+container.appendChild(element);
+}
+}
+function clearGrid(){
+    container.innerHTML="";
+}
+function newGrid(){
+    clearGrid();
+    let selection = parseInt(prompt('Please enter the new number of grid per side it has to be less than 100'));
+    if(selection>100 || selection < 0){
+        return;
+    }
+    size=selection;
+    setGrid();
+
+}
+setGrid();
